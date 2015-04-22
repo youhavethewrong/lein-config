dc350cd37ed4d146ec1d5d3f50f2f85c3276c6f1
ESC <youhavethewrong@gmail.com>
2015-04-21 21:51:14 -0500

    Initial

---
+++ profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-ancient "0.6.7"]
+                  ]
+                :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
