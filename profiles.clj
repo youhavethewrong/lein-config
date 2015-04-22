b209d7fdb6265136894acae1d1895a728acf5748
ESC <youhavethewrong@gmail.com>
2015-04-21 22:01:59 -0500

    clean up

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,3 @@
-{:user {:plugins [
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.6.7"]
-                  ]
-                :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-ancient "0.6.7"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
