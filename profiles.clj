bd7b8f7e3badb726a5b40e6ab0e565764fd9573c
ESC <youhavethewrong@gmail.com>
2015-04-28 16:05:46 -0500

    Update nrepl and add refactor-nrepl.

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.6.7"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+                  [lein-ancient "0.6.7"]
+                  [refactor-nrepl "1.0.3"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
