df0b8778df0c0422e55da73142ed31306c86d969
esc <esc@youhavethewrong.info>
2015-05-07 07:06:57 -0500

    added Austin for cljs

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.6.7"]]
+                  [lein-ancient "0.6.7"]
+                  [com.cemerick/austin "0.1.6"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
