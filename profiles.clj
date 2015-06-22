2dd979d26bdbdd2070f956e9c56643642be70cbe
ESC <youhavethewrong@gmail.com>
2015-06-22 08:23:55 -0500

    Update plugin versions.

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.9.0"]
                   [lein-ancient "0.6.7"]
                   [com.cemerick/austin "0.1.6"]
-                  [refactor-nrepl "1.0.3"]]
+                  [refactor-nrepl "1.0.5"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
 
