8ac7c72359fadf1e99bee869f49f7f44847a1535
ESC <erick.crager@qg.com>
2015-11-20 15:58:39 -0600

    Update tools.

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0"]
-                  [lein-ancient "0.6.7"]
-                  [com.cemerick/austin "0.1.6"]
-                  [refactor-nrepl "1.0.5"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+                  [lein-ancient "0.6.8"]
+                  [refactor-nrepl "1.1.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.11"]]}}
 
