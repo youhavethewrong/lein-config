c9719016f2bda2009e4b35082955f86d0306a89a
ESC <erick.crager@qg.com>
2015-11-20 16:14:08 -0600

    Latest cider-nrepl and newer nrepl.

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [lein-ancient "0.6.8"]
                   [refactor-nrepl "1.1.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.11"]]}}
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
 
