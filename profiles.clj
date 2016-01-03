7d20f02196c8e9ed2c2b58199a7b8d1c6288bceb
negated <youhavethewrong@gmail.com>
2016-01-02 18:43:31 -0600

    Add mirror

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,7 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.10.0"]
                   [lein-ancient "0.6.8"]
                   [refactor-nrepl "1.1.0"]]
+        :mirrors {#"clojars" {:name "clojars mirror"
+                              :url "https://clojars-mirror.tcrawley.org/repo/"}}
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
 
