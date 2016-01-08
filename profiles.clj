c11e4b1589471349c2ce55801b5cbbe645964bbc
negated <negated@ecik.youhavethewrong.info>
2016-01-08 15:07:28 +0000

    Update to snaps of cider-nrepl and refactor-nrepl.

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0"]
+{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [lein-ancient "0.6.8"]
-                  [refactor-nrepl "1.1.0"]]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
         :mirrors {#"clojars" {:name "clojars mirror"
                               :url "https://clojars-mirror.tcrawley.org/repo/"}}
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
