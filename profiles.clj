74b544d5d38003d4dbff8132f84abe0c39d35769
ESC <youhavethewrong@gmail.com>
2016-03-28 19:28:43 -0500

    No longer need to set cider-nrepl and refactor-nrepl deps with the current version of cider.

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.11.0"]
-                  [lein-ancient "0.6.8"]
-                  [refactor-nrepl "2.2.0-SNAPSHOT"]]
+{:user {:plugins [[lein-ancient "0.6.8"]]
         :mirrors {#"clojars" {:name "clojars mirror"
                               :url "https://clojars-mirror.tcrawley.org/repo/"}}
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+        }}
 
