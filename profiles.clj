4ab9708d3d2833dadcb271a9ff547ebdb364e0d7
ESC <youhavethewrong@gmail.com>
2016-04-28 15:11:12 -0500

    Actually, we do need to set them.

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,7 @@
-{:user {:plugins [[lein-ancient "0.6.8"]]
+{:user {:plugins [[lein-ancient "0.6.8"]
+                  [cider/cider-nrepl "0.12.0"]
+                  [refactor-nrepl "2.2.0"]
+                  [jonase/eastwood "0.2.3"]]
         :mirrors {#"clojars" {:name "clojars mirror"
                               :url "https://clojars-mirror.tcrawley.org/repo/"}}
         }}
-
