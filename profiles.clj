486d80bf7f88635bd4378d2a9eaa7973fe8f7e33
ESC <youhavethewrong@gmail.com>
2016-07-31 10:01:27 -0500

    Latest release of cider-nrepl.

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-ancient "0.6.8"]
-                  [cider/cider-nrepl "0.12.0"]
+                  [cider/cider-nrepl "0.13.0"]
                   [refactor-nrepl "2.2.0"]
                   [jonase/eastwood "0.2.3"]]
         :mirrors {#"clojars" {:name "clojars mirror"
