2d5dc32042495326c77367a146b30c476ecb27ce
ESC <youhavethewrong@gmail.com>
2016-09-16 15:42:01 +0000

    Latest snap of clj-refactor

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-ancient "0.6.8"]
                   [cider/cider-nrepl "0.13.0"]
-                  [refactor-nrepl "2.2.0"]
+                  [refactor-nrepl "2.3.0-SNAPSHOT"]
                   [jonase/eastwood "0.2.3"]]
         :mirrors {#"clojars" {:name "clojars mirror"
                               :url "https://clojars-mirror.tcrawley.org/repo/"}}
