8a45584514c2550db7b5ef4a33790bcc81c9629b
ESC <youhavethewrong@gmail.com>
2016-10-28 09:47:11 -0500

    lein-try to try out libraries, lein-exec for scripting

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[lein-ancient "0.6.8"]
-                  [cider/cider-nrepl "0.13.0"]
+                  [cider/cider-nrepl "0.14.0-SNAPSHOT"]
+                  [lein-try "0.4.3"]
+                  [lein-exec "0.3.6"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
                   [jonase/eastwood "0.2.3"]]
         :mirrors {#"clojars" {:name "clojars mirror"
