56d1257f7963942f3c4ef832331f20ff7263afa0
ESC <youhavethewrong@gmail.com>
2016-12-02 15:23:13 +0000

    Latest cider

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-ancient "0.6.8"]
-                  [cider/cider-nrepl "0.14.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.15.0-SNAPSHOT"]
                   [lein-try "0.4.3"]
                   [lein-exec "0.3.6"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
