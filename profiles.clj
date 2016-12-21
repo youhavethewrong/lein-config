1e7464903effcf5eee7bfe8d84e5a38eebee3815
ESC <youhavethewrong@gmail.com>
2016-12-21 15:25:00 -0600

    Add useful helpers

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[lein-ancient "0.6.8"]
                   [cider/cider-nrepl "0.15.0-SNAPSHOT"]
+                  [slamhound "1.5.5"]
+                  [lein-kibit "0.1.3"]
                   [lein-try "0.4.3"]
                   [lein-exec "0.3.6"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
