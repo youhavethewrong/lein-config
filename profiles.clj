fa9c6ef3d7582199f0b09024e395d35433704d4b
ESC <youhavethewrong@gmail.com>
2017-07-28 13:27:16 -0500

    use release

--- profiles.clj
+++ profiles.clj
@@ -3,7 +3,7 @@
                   [slamhound "1.5.5"]
                   [lein-try "0.4.3"]
                   [lein-exec "0.3.6"]
-                  [refactor-nrepl "2.3.0-SNAPSHOT"]
+                  [refactor-nrepl "2.3.1"]
                   [lein-kibit "0.1.3"]
                   [jonase/eastwood "0.2.3"]
                   [lein-bikeshed "0.4.1"]]}}
