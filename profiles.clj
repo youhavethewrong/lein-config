68507950944c0942337ff3c62ebe68d88b4918d6
ESC <youhavethewrong@gmail.com>
2017-10-04 16:26:06 -0500

    Update plugins.

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,10 @@
-{:user {:plugins [[lein-ancient "0.6.8"]
-                  [cider/cider-nrepl "0.15.0"]
+{:user {:plugins [[lein-ancient "0.6.12"]
+                  [cider/cider-nrepl "0.15.1"]
                   [slamhound "1.5.5"]
                   [lein-try "0.4.3"]
                   [lein-exec "0.3.6"]
-                  [refactor-nrepl "2.3.1"]
-                  [lein-kibit "0.1.3"]
-                  [jonase/eastwood "0.2.3"]
-                  [lein-bikeshed "0.4.1"]]}}
+                  [refactor-nrepl "2.3.0-SNAPSHOT"]
+                  [lein-kibit "0.1.5"]
+                  [jonase/eastwood "0.2.4"]
+                  [lein-bikeshed "0.4.1"]]
+        }}
