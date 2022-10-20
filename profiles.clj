032de81f514f75539059a0d9c92b10bde7e01754
Erick Crager <erick@youhavethewrong.info>
2022-10-19 21:34:52 -0500

    Get rid of old stuff and upgrade to the current versions of the
    important things.

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,2 @@
-{:user {:plugins [
-                  [lein-ancient "0.6.15"]
-                  [cider/cider-nrepl "0.21.0"]
-                  [refactor-nrepl "2.4.0"]
-                  [slamhound "1.5.5"]
-                  [lein-try "0.4.3"]
-                  [lein-exec "0.3.7"]
-                  [lein-kibit "0.1.6"]
-                  [jonase/eastwood "0.2.5"]
-                  [lein-bikeshed "0.5.1"]
-                  [lein-nvd "0.5.1"]
-                  [lein-cloverage "1.0.10"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.28.6"]
+                  [refactor-nrepl "3.5.5"]]}}
