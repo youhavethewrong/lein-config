8dd6f5929b6a1eb23a1e9f81bb11fc8651508606
ESC <erick@youhavethewrong.info>
2023-09-05 20:20:24 -0500

    Update and remove plugins I don't care about anymore

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.28.6"]
-                  [refactor-nrepl "3.5.5"]]}}
+{:user {:plugins [[lein-ancient "0.7.0"] ;; upgrade deps
+                  [com.github.clj-kondo/lein-clj-kondo "2023.07.13"] ;; linter
+                  [lein-cloverage "1.2.4"] ;; test coverage
+                  [lein-kibit "0.1.8"] ;; another linter
+                  [lein-nvd "2.0.0"] ;; National Vulnerability Database
+                  ]}}
